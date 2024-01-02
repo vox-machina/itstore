2a614f5b6d2f4d9f3b6d25a4a4e1d4009893c795
Ross McDonald <85923+rossajmcd@users.noreply.github.com>
2024-01-02 20:58:03 +0000

    simplify postrepo-memory test

--- test/voxmachina/itstore/postrepo_memory_test.clj
+++ test/voxmachina/itstore/postrepo_memory_test.clj
@@ -1,20 +1,15 @@
 (ns voxmachina.itstore.postrepo-memory-test
   (:require [clojure.test :refer [deftest is testing]]
-            [voxmachina.itstore.core :as core]
             [voxmachina.itstore.postrepo :as its]
             [voxmachina.itstore.postrepo-memory :as itsmem]))
 
 (def pr-mem (itsmem/repo (atom {})))
 
-(defn create-post [id data] (its/create pr-mem id data))
-
-(defn get-post [id] (its/fetch pr-mem id))
-
 (deftest create-post-test
   (testing "Receive correct representation on post creation"
-    (is (= {"1" {:name "CreateTest"}} (create-post "1" {:name "CreateTest"})))))
+    (is (= {"1" {:name "CreateTest"}} (its/create pr-mem "1" {:name "CreateTest"})))))
 
 (deftest fetch-post-test
   (testing "Retrieve a post from post repo"
-    (is (= {:name "CreateTest"} (get-post "1")))))
+    (is (= {:name "CreateTest"} (its/fetch pr-mem "1")))))
 
